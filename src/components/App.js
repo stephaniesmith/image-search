4bb6785753542d160defbbba1428993e8c87ad7b
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-24 19:12:37 -0800

    getting components set up

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchBar from './SearchBar';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar/>
+    </div> 
+  );
+};
+
+App.propTypes = {
+
+};
+
+export default App;
