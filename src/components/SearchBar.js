7c2fe12e61003ecd789a4b631391f5862e03895b
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-24 19:16:01 -0800

    styling

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react';
 export class SearchBar extends Component {
   render() {
     return (
-      <div>
-        Search Bar
+      <div className="ui segment">
+        <form className="ui form">
+          <label>Image Search</label>
+          <input className="field" type="text"/>
+        </form>
       </div>
     );
   }
