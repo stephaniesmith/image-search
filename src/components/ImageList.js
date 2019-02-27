2281332e8057dac2d2b39a82451b6dc337c0ec76
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-27 06:52:01 -0800

    setting up a grid

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
+import './ImageList.css';
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const ImageList = ({ images }) => {
   return (
-    <div>
+    <div className="image-list">
       {images.map(({ id, urls, description }) => <img key={id} src={urls.small} alt={description}/>)}
     </div>
   );
