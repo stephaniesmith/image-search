b30d2bf24cd554999605ec0cd3224631e58ea5ba
Stephanie Smith <stephsmithcode@gmail.com>
2019-02-27 07:16:32 -0800

    refs to get image hights

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,17 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 
 export class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { spans: 0 };
+    this.imageRef = createRef();
+  }
+
   static propTypes = {
     image: PropTypes.object,
   };
+
+  componentDidMount = () => {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  };
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
+  };
+  
   
   render() {
     const { description, urls } = this.props.image;
+    const { spans } = this.state;
 
     return (
-      <div>
-        <img alt={description} src={urls.regular}/>
+      <div style={{ gridRowEnd: `span ${spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular}/>
       </div>
     );
   }
